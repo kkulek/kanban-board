6731312d8d7f7603e4e7f599d13074e030eebe0d
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-09 14:01:51 +0200

    dynamic inputs kept in AddTask state

--- src/components/elements/Input.jsx
+++ src/components/elements/Input.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 
-export function Input() {
+export function Input({name, value, placeholder, onChange}) {
     return (
         <form className="mb-2">
-            <label htmlFor="title"
+            <label htmlFor={name}
                    className="block text-sm font-bold mb-1 text-gray-100 tracking-wider">
                 Title
             </label>
-            <input type="text" placeholder="e.g. Create an e-mail newsletter" id="title"
+            <input type="text" placeholder={placeholder} id={name}
                    className="shadow appearance-none border rounded w-full
                    py-2 px-3 text-gray-700 leading-tight
                    focus:outline-slate-400 focus:shadow-outline"
+                   onChange={onChange}
+                   value={value}
+                   name={name}
             />
         </form>
     )
