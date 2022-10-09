c91cdb85bd8fa8406128999d58da8a33a3b73039
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-09 19:38:39 +0200

    passing task to DisplayTask component

---
+++ src/components/tasks/DisplayTask.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export function DisplayTask(task){
+        return (
+            console.log(task)
+
+        )
+}
--- src/components/tasks/SmallTaskCard.jsx
+++ src/components/tasks/SmallTaskCard.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import {DisplayTask} from "./DisplayTask";
 
 export function SmallTaskCard({taskList}){
+    function openTaskInModal(task) {
+        DisplayTask(task)
+    }
+
     return (
     <div className="flex-col">
-        {taskList.map(x => (
-            <div className="px-4 py-2 my-5 bg-blue-400">
-                <h3 className="font-bold">{x.title}</h3>
-                <p className="font-light">{x.description}</p>
+        {taskList.map(task => (
+            <div key={task.id}
+                 onClick={() => openTaskInModal(task)}
+                className="px-4 py-2 my-5 bg-blue-400 cursor-pointer">
+                <h3 className="font-bold">{task.title}</h3>
+                <p className="font-light">{task.description}</p>
             </div>
         ))}
     </div>
