438f13e57677238127b75eb67ba2a5778768cce4
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-09 20:44:40 +0200

    basic setup

--- src/components/tasks/DisplayTask.jsx
+++ src/components/tasks/DisplayTask.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import Modal from "react-modal";
 
-export function DisplayTask(task){
-        return (
-            console.log(task)
+export function DisplayTask({visible, task, onClose}) {
+    if(!visible) return null;
 
-        )
+    return (
+        <div>
+            <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm
+            flex justify-center items-center">
+                <div className="bg-white p-2 rounded">
+                    <h3>Tytuł: {task.title}</h3>
+                    <p>Opis: {task.description}</p>
+                    <p>ID: {task.id}</p>
+                    <p>Subtask: {task.subtasks}</p>
+                    <button onClick={onClose}>X</button>
+                </div>
+            </div>
+        </div>
+    )
 }
