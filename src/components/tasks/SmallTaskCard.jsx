dda2e282ab7364d034d29bcd11ad51b7be629521
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-09 20:44:55 +0200

    onClick show task modal

--- src/components/tasks/SmallTaskCard.jsx
+++ src/components/tasks/SmallTaskCard.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import {DisplayTask} from "./DisplayTask";
 
-export function SmallTaskCard({taskList}){
-    function openTaskInModal(task) {
-        DisplayTask(task)
+export function SmallTaskCard({taskList}) {
+    const [showTask, setShowTask] = useState(false);
+
+    function openTaskInModal() {
+        setShowTask(true)
+    }
+
+    function handleOnClose() {
+        setShowTask(false)
     }
 
     return (
-    <div className="flex-col">
-        {taskList.map(task => (
-            <div key={task.id}
-                 onClick={() => openTaskInModal(task)}
-                className="px-4 py-2 my-5 bg-blue-400 cursor-pointer">
-                <h3 className="font-bold">{task.title}</h3>
-                <p className="font-light">{task.description}</p>
-            </div>
-        ))}
-    </div>
+        <>
+        <div className="flex-col">
+            {taskList.map(task => (
+                <div key={task.id}
+                     onClick={() => openTaskInModal()}
+                     className="px-4 py-2 my-5 bg-blue-400 cursor-pointer">
+                    <h3 className="font-bold">{task.title}</h3>
+                    <p className="font-light">{task.description}</p>
+                    <DisplayTask
+                        onClose={handleOnClose}
+                        visible={showTask}
+                        task={task}/>
+                </div>
+            ))}
+        </div>
+        </>
     )
 }
