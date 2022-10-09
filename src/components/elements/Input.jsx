f69b7e61917e98c32ce5d91381fd7bf14528fa25
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-09 14:16:28 +0200

    deleted label from component

--- src/components/elements/Input.jsx
+++ src/components/elements/Input.jsx
@@ -3,10 +3,6 @@ import React from "react";
 export function Input({name, value, placeholder, onChange}) {
     return (
         <form className="mb-2">
-            <label htmlFor={name}
-                   className="block text-sm font-bold mb-1 text-gray-100 tracking-wider">
-                Title
-            </label>
             <input type="text" placeholder={placeholder} id={name}
                    className="shadow appearance-none border rounded w-full
                    py-2 px-3 text-gray-700 leading-tight
