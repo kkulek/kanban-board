49ea81e7ee942eb39381edd2ecfc8dd0d984c491
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-09 19:11:26 +0200

    save input in taskList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import {AddTask} from "./components/tasks/AddTask";
+import {useEffect, useState} from "react";
 
 function App() {
+    const [taskList, setTaskList] = useState([]);
+
+    function addToTaskList(newTask) {
+        setTaskList(prevState => [...prevState, newTask])
+        console.log(taskList)
+    }
+
+
   return (
       <div className="bg-gray-500 p-4">
-        <AddTask />
+        <AddTask addToTaskList={addToTaskList} />
       </div>
   )
 }
--- src/components/elements/Button.jsx
+++ src/components/elements/Button.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-export function Button({cta, type, color}){
+export function Button({cta, type, color, submit}){
     return (
-    <button type={type} onClick={(e) => e.preventDefault()}
+    <button type={type} onClick={(e) => {
+        e.preventDefault();
+        submit()
+    }}
             className={`bg-${color}-500 rounded text-white px-2 py-1`}
     >{cta}</button>
     )
