147699327b5d2aec56641365a91071abf3521e16
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-09 13:10:44 +0200

    basic input styles

---
+++ src/components/elements/Input.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export function Input() {
+    return (
+        <>
+            <label htmlFor="title"
+                   className="block text-sm font-bold mb-1 text-gray-100 tracking-wider">
+                Title
+            </label>
+            <input type="text" placeholder="e.g. Create an e-mail newsletter" id="title"
+                   className="shadow appearance-none border rounded w-full
+                   py-2 px-3 text-gray-700 leading-tight
+                   focus:outline-slate-400 focus:shadow-outline"
+            />
+        </>
+    )
+}
