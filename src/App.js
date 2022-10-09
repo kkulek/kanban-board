fa4850e36d122cd1402d0cef2af6123f46676626
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-09 19:24:20 +0200

    added SmallTaskCard component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {AddTask} from "./components/tasks/AddTask";
 import {useEffect, useState} from "react";
+import {SmallTaskCard} from "./components/tasks/SmallTaskCard";
 
 function App() {
     const [taskList, setTaskList] = useState([]);
@@ -9,10 +10,10 @@ function App() {
         console.log(taskList)
     }
 
-
   return (
       <div className="bg-gray-500 p-4">
         <AddTask addToTaskList={addToTaskList} />
+          <SmallTaskCard taskList={taskList}/>
       </div>
   )
 }
