1ac70f4b368194423d017ca008f98ab77881e5f0
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-09 14:59:23 +0200

    added subtasks inputs with label

--- src/components/tasks/AddTask.jsx
+++ src/components/tasks/AddTask.jsx
@@ -1,12 +1,13 @@
 import React, {useState} from "react";
 import {Input} from "../elements/Input";
 import {Label} from "../elements/Label";
+import {Button} from "../elements/Button";
 
 export function AddTask(){
     const [input, setInput] = useState({
         title: '',
         description: '',
-        subtasks: [{}],
+        subtasks: [],
         id: '',
         column: ''
     });
@@ -21,7 +22,7 @@ export function AddTask(){
     }
 
     return (
-    <div>
+    <form>
         <Label label="Title" htmlFor="title"/>
         <Input name="title" value={input.title} placeholder="e.g. Create an e-mail newsletter"
                onChange={handleInput}/>
@@ -29,6 +30,15 @@ export function AddTask(){
         <Label label="Description" htmlFor="description"/>
         <Input name="description" value={input.description} placeholder="e.g. Black Friday discount with a company's video"
                onChange={handleInput}/>
-    </div>
+
+        <Label label="Subtasks" htmlFor="subtasks"/>
+        <Input name="subtasks" value={input.subtasks} placeholder="e.g. Choose a title"
+               onChange={handleInput}/>
+
+        <Input name="subtasks" value={input.subtasks} placeholder="e.g. Write e-mail body"
+               onChange={handleInput}/>
+
+        <Button type="submit" cta="Create Task" color="blue"/>
+    </form>
     )
 }
