28e6f80401fad919424ab4ac69caa8efee4181a4
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-09 14:18:17 +0200

    added Label component, input object with fields

--- src/components/tasks/AddTask.jsx
+++ src/components/tasks/AddTask.jsx
@@ -1,10 +1,14 @@
 import React, {useState} from "react";
 import {Input} from "../elements/Input";
+import {Label} from "../elements/Label";
 
 export function AddTask(){
     const [input, setInput] = useState({
         title: '',
-        description: ''
+        description: '',
+        subtasks: [{}],
+        id: '',
+        column: ''
     });
 
     function handleInput(event) {
@@ -18,9 +22,11 @@ export function AddTask(){
 
     return (
     <div>
+        <Label label="Title" htmlFor="title"/>
         <Input name="title" value={input.title} placeholder="e.g. Create an e-mail newsletter"
                onChange={handleInput}/>
 
+        <Label label="Description" htmlFor="description"/>
         <Input name="description" value={input.description} placeholder="e.g. Black Friday discount with a company's video"
                onChange={handleInput}/>
     </div>
