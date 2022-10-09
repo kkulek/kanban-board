08f4294435fc398b312f83e28731401928c8e2c9
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-09 13:21:31 +0200

    basic setup react func. component

---
+++ src/components/tasks/AddTask.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Input} from "../elements/Input";
+
+export function AddTask(){
+    return (
+    <div>
+        <Input />
+        <Input />
+        <Input />
+    </div>
+    )
+}
