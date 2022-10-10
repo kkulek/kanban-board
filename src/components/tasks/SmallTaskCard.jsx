e65ffbf30c5100977c9d5cd7b4278be45c6bce16
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-10 12:53:30 +0200

    reading tasks from firebase

--- src/components/tasks/SmallTaskCard.jsx
+++ src/components/tasks/SmallTaskCard.jsx
@@ -21,18 +21,17 @@ export function SmallTaskCard({taskList}) {
                         <div key={task.id}
                              onClick={() => openTaskInModal(task)}
                              className="px-4 py-2 my-5 bg-blue-400 cursor-pointer">
-                            <h3 className="font-bold">{task.title}</h3>
-                            <p className="font-light">{task.description}</p>
+                            <h3 className="font-bold">{task.input.title}</h3>
+                            <p className="font-light">{task.input.description}</p>
                         </div>
                         {showTask && (
                                 <div id="task-modal" key={task.title} onClick={handleOnClose} className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm
                                     flex justify-center items-center">
                                     <div className="bg-white p-2 rounded">
-                                        <h3>Tytuł: {task.title}</h3>
-                                        <p>Opis: {task.description}</p>
+                                        <h3>Tytuł: {task.input.title}</h3>
+                                        <p>Opis: {task.input.description}</p>
                                         <p>ID: {task.id}</p>
-                                        <p>Subtask: {task.subtasks}</p>
-                                        <h1>asd</h1>
+                                        <p>Subtask: {task.input.subtasks}</p>
                                     </div>
                                 </div>
                         )}
