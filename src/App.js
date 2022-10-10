79a1a3627c750df8f107a94f7c15a4aea67a7f20
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-10 12:53:57 +0200

    importing and displaying tasks from database

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import {AddTask} from "./components/tasks/AddTask";
 import {useEffect, useState} from "react";
 import {SmallTaskCard} from "./components/tasks/SmallTaskCard";
+import {db} from "./firebase";
+import {collection, query, onSnapshot} from 'firebase/firestore'
 
 function App() {
     const [taskList, setTaskList] = useState([]);
 
+    useEffect(() => {
+        const q = query(collection(db, "todos"));
+        const unsub = onSnapshot(q, (querySnapshot) => {
+            let todosArray = [];
+            querySnapshot.forEach((doc) => {
+                todosArray.push({
+                    ...doc.data(),
+                    id: doc.id,
+                    title: doc.title
+                });
+            });
+            setTaskList(todosArray)
+        });
+        return () => unsub();
+    }, []);
+
     function addToTaskList(newTask) {
         setTaskList(prevState => [...prevState, newTask])
-        console.log(taskList)
     }
 
-  return (
-      <div className="bg-gray-500 p-4">
-        <AddTask addToTaskList={addToTaskList} />
-          <SmallTaskCard taskList={taskList}/>
-      </div>
-  )
+    return (
+        <div className="bg-gray-500 p-4">
+            <AddTask addToTaskList={addToTaskList}/>
+            <SmallTaskCard taskList={taskList}/>
+        </div>
+    )
 }
 
 export default App;
