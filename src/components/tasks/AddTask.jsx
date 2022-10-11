1b8b09e0d6c56d2a6f36ca51642047ffb4dafaf5
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-11 15:29:21 +0200

    temp solution for adding subtasks array

--- src/components/tasks/AddTask.jsx
+++ src/components/tasks/AddTask.jsx
@@ -8,12 +8,12 @@ import {db} from "../../firebase";
 import {collection, addDoc} from 'firebase/firestore'
 import {Subtask} from "./Subtask";
 
-export function AddTask(){
+export function AddTask() {
     const DEFAULT_TASK = {
-            title: '',
-            description: '',
-            subtasks: [],
-            column: ''
+        title: '',
+        description: '',
+        subtasks: [{name: ""}, {name: ""}],
+        column: ''
     }
 
     const [input, setInput] = useState(DEFAULT_TASK);
@@ -28,6 +28,24 @@ export function AddTask(){
         }))
     }
 
+    const handleSubtaskChange = index => event => {
+        const newSubtasks = input.subtasks.map((subtask, sindex) => {
+            if (index !== sindex) return subtask;
+            return {...subtask, name: event.target.value, id: uuidv4(), completed: false};
+        });
+
+        setInput(prevState => (
+            {
+                ...prevState,
+                subtasks: newSubtasks
+            }
+        ))
+    }
+
+    function handleAddSubtask() {
+        setInput({subtasks: input.subtasks.concat({ name: "" })});
+    }
+
     const handleSubmit = async () => {
         if (input !== "") {
             await addDoc(collection(db, "todos"), {
@@ -39,22 +57,30 @@ export function AddTask(){
     }
 
     return (
-    <form>
-        <Label label="Title" htmlFor="title"/>
-        <Input name="title" value={input.title} placeholder="e.g. Create an e-mail newsletter"
-               onChange={handleInput}/>
+        <form>
+            <Label label="Title" htmlFor="title"/>
+            <Input name="title" value={input.title} placeholder="e.g. Create an e-mail newsletter"
+                   onChange={handleInput}/>
 
-        <Label label="Description" htmlFor="description"/>
-        <Input name="description" value={input.description} placeholder="e.g. Black Friday discount with a company's video"
-               onChange={handleInput}/>
+            <Label label="Description" htmlFor="description"/>
+            <Input name="description" value={input.description}
+                   placeholder="e.g. Black Friday discount with a company's video"
+                   onChange={handleInput}/>
 
-        <Label label="Subtasks" htmlFor="subtasks"/>
-        {/*<Input name="subtasks" value={input.subtasks} placeholder="e.g. Choose a title"*/}
-        {/*       onChange={handleInput}/>*/}
+            <Label label="Subtasks" htmlFor="subtasks"/>
+            {input.subtasks.map((subtask, index) => (
+                <Input key={index} type="text" placeholder={`Subtask ${index + 1}`}
+                       value={subtask.name}
+                       onChange={handleSubtaskChange(index)}
+                />
+            ))}
 
-        <Subtask name="subtasks" onChange={handleInput} />
+            <button type="button" onClick={handleAddSubtask}>Add subtask</button>
 
-        <Button type="submit" cta="Create Task" color="blue" submit={handleSubmit}/>
-    </form>
+            {/*<Subtask name="subtasks" onChange={handleInput}/>*/}
+
+
+            <Button type="submit" cta="Create Task" color="blue" submit={handleSubmit}/>
+        </form>
     )
 }
