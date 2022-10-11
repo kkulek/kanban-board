7c7eb167c14daaf9e7e3d85c9de56d69da8bfaf6
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-11 16:55:39 +0200

    working functionality

--- src/components/tasks/Subtask.jsx
+++ src/components/tasks/Subtask.jsx
@@ -1,32 +1,28 @@
 import React, {useState} from "react";
 import {Input} from "../elements/Input";
 
-export function Subtask({name, onChange, value}) {
-    const [addSubtask, setAddSubtask] = useState([]);
+export function Subtask({name, onChange}) {
+    const [addSubtaskField, setAddSubtaskField] = useState([]);
 
-    const subtaskId = () => addSubtask.length + 1
+    const subtaskId = () => addSubtaskField.length + 1
 
     function handleOnClick(e) {
         e.preventDefault()
-        setAddSubtask(prevState => [...prevState, {
-            id: subtaskId(),
-        }])
+        setAddSubtaskField(prevState => [...prevState, subtaskId()])
     }
 
     return (
         <>
-            <Input name={name}
+            <Input name={name + 1}
                    onChange={onChange}
-                   value={value}
                    placeholder="e.g. Choose a title"/>
 
-            <Input name={name}
+            <Input name={name + 2}
                    onChange={onChange}
-                   value={value}
                    placeholder="e.g. Write an e-mail body"/>
 
-            {addSubtask.map((subtask, index) => (
-                    <Input key={index} name={name} onChange={onChange} value={value} placeholder="Another subtask." />
+            {addSubtaskField.map((subtask, index) => (
+                    <Input key={index} name={name + (index + 3)} onChange={onChange} placeholder="Another subtask." />
             ))}
             <button onClick={handleOnClick}>Add subtask</button>
         </>
