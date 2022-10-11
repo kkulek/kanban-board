e845d9440d56d326b3d5d724be43e6b5dadb88fc
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-11 16:01:42 +0200

    styling: flex for subtask and delete

--- src/components/tasks/AddTask.jsx
+++ src/components/tasks/AddTask.jsx
@@ -76,15 +76,19 @@ export function AddTask() {
 
             <Label label="Subtasks" htmlFor="subtasks"/>
             {input.subtasks.map((subtask, index) => (
-                <div key={index}>
+                <div key={index} className="flex gap-2 mb-3">
                 <Input type="text" placeholder={`Subtask ${index + 1}`}
                        value={subtask.name}
                        onChange={handleSubtaskChange(index)}/>
                 <button type="button" onClick={handleRemoveSubtask(index)}>X</button>
                 </div>
             ))}
-            <button type="button" onClick={handleAddSubtask}>Add subtask</button>
+            <button type="button"
+                    onClick={handleAddSubtask}
+                    className="block w-full bg-white rounded-2xl px-3 py-2"
+            >+ Add New Subtask</button>
 
+            <h3>Placeholder for status</h3>
             {/*<Subtask name="subtasks" onChange={handleInput}/>*/}
 
             <Button type="submit" cta="Create Task" color="blue" submit={handleSubmit}/>
