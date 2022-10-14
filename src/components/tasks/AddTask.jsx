9cbf6d8d48e762a4b7ac8fec3edfd0821adfb0e9
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-14 14:17:41 +0200

    deleted whitespaces

--- src/components/tasks/AddTask.jsx
+++ src/components/tasks/AddTask.jsx
@@ -77,37 +77,34 @@ export function AddTask() {
         <>
             <button onClick={openAddTaskInModal}>Add Task</button>
             {showAddTask && (
-
                 <Modal onClick={handleOnClose}>
-                        <form>
-                            <Label label="Title" htmlFor="title"/>
-                            <Input name="title" value={input.title} placeholder="e.g. Create an e-mail newsletter"
-                                   onChange={handleInput}/>
+                    <form>
+                        <Label label="Title" htmlFor="title"/>
+                        <Input name="title" value={input.title} placeholder="e.g. Create an e-mail newsletter"
+                               onChange={handleInput}/>
 
-                            <Label label="Description" htmlFor="description"/>
-                            <Input name="description" value={input.description}
-                                   placeholder="e.g. Black Friday discount with a company's video"
-                                   onChange={handleInput}/>
+                        <Label label="Description" htmlFor="description"/>
+                        <Input name="description" value={input.description}
+                               placeholder="e.g. Black Friday discount with a company's video"
+                               onChange={handleInput}/>
 
-                            <Label label="Subtasks" htmlFor="subtasks"/>
-                            {input.subtasks.map((subtask, index) => (
-                                <div key={index} className="flex gap-2 mb-3">
-                                    <Input type="text" placeholder={`Subtask ${index + 1}`}
-                                           value={subtask.name}
-                                           onChange={handleSubtaskChange(index)}/>
-                                    <button type="button" onClick={handleRemoveSubtask(index)}>X</button>
-                                </div>
-                            ))}
-                            <button type="button"
-                                    onClick={handleAddSubtask}
-                                    className="block w-full bg-white rounded-2xl px-3 py-2 text-black"
-                            >+ Add New Subtask
-                            </button>
-
-                            <h3>Placeholder for status</h3>
-                            <Button type="submit" cta="Create Task" color="blue" submit={handleSubmit}/>
-                        </form>
-
+                        <Label label="Subtasks" htmlFor="subtasks"/>
+                        {input.subtasks.map((subtask, index) => (
+                            <div key={index} className="flex gap-2 mb-3">
+                                <Input type="text" placeholder={`Subtask ${index + 1}`}
+                                       value={subtask.name}
+                                       onChange={handleSubtaskChange(index)}/>
+                                <button type="button" onClick={handleRemoveSubtask(index)}>X</button>
+                            </div>
+                        ))}
+                        <button type="button"
+                                onClick={handleAddSubtask}
+                                className="block w-full bg-white rounded-2xl px-3 py-2 text-black"
+                        >+ Add New Subtask
+                        </button>
+                        <h3>Placeholder for status</h3>
+                        <Button type="submit" cta="Create Task" color="blue" submit={handleSubmit}/>
+                    </form>
                 </Modal>
             )}
         </>
