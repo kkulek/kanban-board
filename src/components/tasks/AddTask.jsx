78bedcf9fae67d59bf365c047cd92a0e9963b956
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-14 13:53:15 +0200

    add task opening in modal

--- src/components/tasks/AddTask.jsx
+++ src/components/tasks/AddTask.jsx
@@ -6,6 +6,7 @@ import {v4 as uuidv4} from "uuid";
 
 import {db} from "../../firebase";
 import {collection, addDoc} from 'firebase/firestore'
+import {DisplayTaskModal} from "./DisplayTaskModal";
 
 export function AddTask() {
     const DEFAULT_TASK = {
@@ -62,35 +63,60 @@ export function AddTask() {
         }
     }
 
+    const [showAddTask, setShowAddTask] = useState(false);
+
+
+    function openAddTaskInModal() {
+        setShowAddTask(true)
+    }
+
+    function handleOnClose(event) {
+        if (event.target.id === 'task-modal')
+            setShowAddTask(false)
+    }
+
     return (
-        <form>
-            <Label label="Title" htmlFor="title"/>
-            <Input name="title" value={input.title} placeholder="e.g. Create an e-mail newsletter"
-                   onChange={handleInput}/>
+        <>
+            <button onClick={openAddTaskInModal}>Add Task</button>
+            {showAddTask && (
+                <div id="task-modal" onClick={handleOnClose}
+                     className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm
+             flex justify-center items-center">
 
-            <Label label="Description" htmlFor="description"/>
-            <Input name="description" value={input.description}
-                   placeholder="e.g. Black Friday discount with a company's video"
-                   onChange={handleInput}/>
+                    <div className="bg-gray-700 p-2 rounded w-4/5 text-white flex-col gap-5 p-8 max-w-lg">
 
-            <Label label="Subtasks" htmlFor="subtasks"/>
-            {input.subtasks.map((subtask, index) => (
-                <div key={index} className="flex gap-2 mb-3">
-                <Input type="text" placeholder={`Subtask ${index + 1}`}
-                       value={subtask.name}
-                       onChange={handleSubtaskChange(index)}/>
-                <button type="button" onClick={handleRemoveSubtask(index)}>X</button>
+                    <form>
+                        <Label label="Title" htmlFor="title"/>
+                        <Input name="title" value={input.title} placeholder="e.g. Create an e-mail newsletter"
+                               onChange={handleInput}/>
+
+                        <Label label="Description" htmlFor="description"/>
+                        <Input name="description" value={input.description}
+                               placeholder="e.g. Black Friday discount with a company's video"
+                               onChange={handleInput}/>
+
+                        <Label label="Subtasks" htmlFor="subtasks"/>
+                        {input.subtasks.map((subtask, index) => (
+                            <div key={index} className="flex gap-2 mb-3">
+                                <Input type="text" placeholder={`Subtask ${index + 1}`}
+                                       value={subtask.name}
+                                       onChange={handleSubtaskChange(index)}/>
+                                <button type="button" onClick={handleRemoveSubtask(index)}>X</button>
+                            </div>
+                        ))}
+                        <button type="button"
+                                onClick={handleAddSubtask}
+                                className="block w-full bg-white rounded-2xl px-3 py-2 text-black"
+                        >+ Add New Subtask
+                        </button>
+
+                        <h3>Placeholder for status</h3>
+                        {/*<Subtask name="subtasks" onChange={handleInput}/>*/}
+                        <Button type="submit" cta="Create Task" color="blue" submit={handleSubmit}/>
+                    </form>
+                    </div>
                 </div>
-            ))}
-            <button type="button"
-                    onClick={handleAddSubtask}
-                    className="block w-full bg-white rounded-2xl px-3 py-2"
-            >+ Add New Subtask</button>
-
-            <h3>Placeholder for status</h3>
-            {/*<Subtask name="subtasks" onChange={handleInput}/>*/}
-
-            <Button type="submit" cta="Create Task" color="blue" submit={handleSubmit}/>
-        </form>
+            )}
+        </>
     )
 }
