6df7f095902a943f804f6e4c501f90b3d116de10
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-14 20:21:25 +0200

    added GlobalProvider

--- src/components/tasks/DisplayTaskModal.jsx
+++ src/components/tasks/DisplayTaskModal.jsx
@@ -1,6 +1,7 @@
-import React, {useState} from "react";
+import React from "react";
 import {Modal} from "../elements/Modal";
 import {EditTask} from "./EditTask";
+import {GlobalProvider} from "../../GlobalState";
 
 export function DisplayTaskModal({task, handleOnClose, showTask, handleDelete, editTask, handleEdit}) {
     if (!showTask) return null;
@@ -8,37 +9,36 @@ export function DisplayTaskModal({task, handleOnClose, showTask, handleDelete, e
     return (
         <Modal onClick={handleOnClose}>
 
-            {!editTask ? (
-                <>
-                    <div className="flex justify-between items-center">
-                        <h3 className="font-bold text-xl mr-2">
-                            {task.input.title}</h3>
-                        <div>
-                            <button className="block text-xs mb-2" onClick={() => handleDelete(task.id)}>delete</button>
-                            <button className="block text-xs" onClick={handleEdit}>edit</button>
+            {editTask ? (
+                    <GlobalProvider task={task}>
+                        <EditTask handleOnClose={handleOnClose} task={task}/>
+                    </GlobalProvider>
+                )
+                : (
+                    <div key={task.id}>
+                        <div className="flex justify-between items-center">
+                            <h3 className="font-bold text-xl mr-2">
+                                {task.input.title}</h3>
+                            <div>
+                                <button className="block text-xs mb-2" onClick={() => handleDelete(task.id)}>delete
+                                </button>
+                                <button className="block text-xs" onClick={handleEdit}>edit</button>
+                            </div>
                         </div>
-
+                        <p className="font-light text-base mt-4 text-gray-300">{task.input.description}</p>
+                        <p className="font-bold mt-4">Subtask ( of {task.input.subtasks.length})</p>
+                        {task.input.subtasks.map(subtask => (
+                            <div className="mt-2"
+                                 key={subtask.name}>
+                                <label className="flex items-center gap-4 bg-gray-800 p-2 text-sm font-bold">
+                                    <input type="checkbox" className="h-4 w-4 rounded-full shadow"/>
+                                    {subtask.name}
+                                </label>
+                            </div>
+                        ))}
+                        <p className="mt-4">Current Status</p>
                     </div>
-                    <p className="font-light text-base mt-4 text-gray-300">{task.input.description}</p>
-                    <p className="font-bold mt-4">Subtask ( of {task.input.subtasks.length})</p>
-                    {task.input.subtasks.map(x => (
-                        <div className="mt-2"
-                             key={x.id}>
-                            <label className="flex items-center gap-4 bg-gray-800 p-2 text-sm font-bold">
-                                <input type="checkbox" className="h-4 w-4 rounded-full shadow"/>
-                                {x.name}
-                            </label>
-                        </div>
-                    ))}
-                    <p className="mt-4">Current Status</p>
-                </>
-            ) : (
-                <EditTask handleOnClose={handleOnClose} task={task}/>
-            ) }
-
-
-
-
+                )}
         </Modal>
     )
 }
