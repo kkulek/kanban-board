28aacf4f01f0611a3568ddb130e15085851a646d
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-19 18:39:25 +0200

    styled component

--- src/components/tasks/SmallTaskCard.jsx
+++ src/components/tasks/SmallTaskCard.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export function SmallTaskCard({task, openTaskInModal}){
     return (
         <div onClick={() => openTaskInModal(task)}
-             className="px-4 py-2 my-5 bg-blue-400 cursor-pointer">
-            <h3 className="font-bold">{task.input.title}</h3>
-            <p className="font-light">{task.input.description}</p>
+             className="px-4 py-2 mt-2 mb-5 bg-gray-700 cursor-pointer rounded">
+            <h3 className="font-bold text-white text-m">{task.input.title}</h3>
+            <p className="font-light text-gray-400 text-sm">{task.input.description}</p>
         </div>
     )
 }
